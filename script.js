78e39c2226c91abd72575e912efabba1e8e42281
rubyred-thorn <110918468+rubyred-thorn@users.noreply.github.com>
2023-07-26 13:41:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,43 @@ function showSlide(n) {
   currentSlide = n;
 }
 
+function saveCheckboxesState() {
+  const checkboxes = slides[currentSlide].querySelectorAll("input[type='checkbox']");
+  const checkboxState = Array.from(checkboxes).map(checkbox => checkbox.checked);
+  localStorage.setItem(`checklist_${currentSlide}`, JSON.stringify(checkboxState));
+}
+
+function loadCheckboxesState() {
+  const checkboxStateJSON = localStorage.getItem(`checklist_${currentSlide}`);
+  if (checkboxStateJSON) {
+    const checkboxState = JSON.parse(checkboxStateJSON);
+    const checkboxes = slides[currentSlide].querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach((checkbox, index) => {
+      checkbox.checked = checkboxState[index];
+      if (checkbox.checked) {
+        checkbox.parentNode.classList.add("checked");
+      } else {
+        checkbox.parentNode.classList.remove("checked");
+      }
+    });
+  }
+}
+
 document.getElementById("prev").addEventListener("click", () => {
   if (currentSlide > 0) {
+    saveCheckboxesState();
     showSlide(currentSlide - 1);
+    loadCheckboxesState();
   }
 });
 
 document.getElementById("next").addEventListener("click", () => {
   if (currentSlide < slides.length - 1) {
+    saveCheckboxesState();
     showSlide(currentSlide + 1);
+    loadCheckboxesState();
   }
 });
 
 showSlide(currentSlide);
+loadCheckboxesState();
